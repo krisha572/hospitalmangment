371b7cf2dd2fe91ba328a1f8e1685c74f6a02aa9
Sunil <memakiyasunil.b11@gmail.com>
2026-07-28 08:47:23 +0530

    chore: add IDE configuration and build output directories to workspace

--- backend/obj/Release/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Release/net8.0/backend.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b47e496a1bb946c77dd68973537b00d773da748a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
